2aa0f7c7cf19003dbe28d71abba67bb90ff258eb
aledguedes <alexandredguedes@gmail.com>
2024-06-21 17:56:42 -0300

    integração com serviço on line

--- src/app/pages/dashboard/dash-content/dash-content.component.ts
+++ src/app/pages/dashboard/dash-content/dash-content.component.ts
@@ -27,6 +27,12 @@ export class DashContentComponent {
       title: "Simple Card 3",
       info: "Lorem ipsum dolor sit amet 3333..."
     }
+  ];
+
+  orders = [
+    { "order_id": 1, "user_id": "Emily", "items": [{ "product_id": "Produto 01", "quantity": 2 }, { "product_id": 3, "quantity": 1 }], "total_price": 849.97, "status": "Shipped" },
+    { "order_id": 2, "user_id": "João", "items": [{ "product_id": "Produto 02", "quantity": 1 }, { "product_id": 7, "quantity": 1 }], "total_price": 1149.98, "status": "Delivered" },
+    { "order_id": 3, "user_id": "Djoor", "items": [{ "product_id": "Produto 04", "quantity": 1 }, { "product_id": 8, "quantity": 1 }], "total_price": 599.98, "status": "Processing" }
   ]
 
 }
